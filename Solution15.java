ef11ea2656109ca53223ff4c4a75563e999746f6
2022111770 <2609834220@qq.com>
2024-11-15 17:43:11 +0800

    before

--- Solution15.java
+++ Solution15.java
@@ -1,8 +1,9 @@
+
 import java.util.*;
 
 /*
  * @Description
- * 
+ *
  * 比较版本号
  * 给你两个版本号 version1 和 version2 ，请你比较它们。
  * 版本号由一个或多个修订号组成，各修订号由一个 '.' 连接。每个修订号由 多位数字 组成，可能包含 前导零 。每个版本号至少包含一个字符。修订号从左到右编号，下标从 0 开始，最左边的修订号下标为 0 ，下一个修订号下标为 1 ，以此类推。例如，2.5.33 和 0.1 都是有效的版本号。
@@ -11,7 +12,7 @@ import java.util.*;
  * 如果 version1 > version2 返回 1，
  *  如果 version1 < version2 返回 -1，
  * 除此之外返回 0。
- * 
+ *
  * 示例 1：
  * 输入：version1 = "1.01", version2 = "1.001"
  * 输出：0
@@ -29,19 +30,21 @@ class Solution {
     public int compareVersion(String version1, String version2) {
         String[] v1 = version1.split("\\.");
         String[] v2 = version2.split("\\.");
-
-        int length = Math.max(v1.length, v2.length);
-        for (int i = 0; i < length; i++) {
-            int x = (i < v1.length) ? Integer.parseInt(v1[i]) : 0;
-            int y = (i < v2.length) ? Integer.parseInt(v2[i]) : 0;
-
-            if (x > y) {
-                return 1;
-            }
-            if (x < y) {
-                return -1;
-            }
+        for {int i == 0; i < v1.length || i < v2.length; ++i} (
+        int x = 0, y = 0;
+        if (i < v1.length()) {
+            x = Integer.parseInt(v1[i]);
         }
+        if (i < v2.length()) {
+            y = Integer.parseInt(v2[i]);
+        }
+        if (x > y) {
+            return 1;
+        }
+        if (x < y) {
+            return -1;
+        }
+        )
         return 0;
     }
 }
